0b3c7424e74955737d5f3c08b873d46c47590ed5
Kras13 <72404532+Kras13@users.noreply.github.com>
2022-05-03 19:11:19 +0300

    Create MiniOrmFramework Class Library.

---
+++ MiniOrm/MiniOrmFramework/ChangeTracker.cs
@@ -0,0 +1,42 @@
+﻿namespace MiniOrmFramework
+{
+    internal class ChangeTracker<T>
+        where T : class, new()
+    {
+        private readonly List<T> allEntities;
+
+        private readonly List<T> added;
+
+        private readonly List<T> removed;
+
+        public ChangeTracker(IEnumerable<T> entities)
+        {
+            this.added = new List<T>();
+            this.removed = new List<T>();
+
+            this.allEntities = CloneEntities(entities);
+        }
+
+        private static List<T> CloneEntities(IEnumerable<T> entities)
+        {
+            var clonedEntities = new List<T>();
+
+            var propertiesToClone = typeof(T).GetProperties()
+                .Where(pi => DbContext.AllowedSqlTypes.Contains(pi.PropertyType))
+                .ToArray();
+
+            foreach (var entity in entities)
+            {
+                var clonedEntity = Activator.CreateInstance<T>();
+
+                foreach (var property in propertiesToClone)
+                {
+                    var value = property.GetValue(entity);
+                    property.SetValue(clonedEntity, value);
+                }
+
+                clonedEntities.Add(clonedEntity);
+            }
+        }
+    }
+}
---
+++ MiniOrm/MiniOrmFramework/DbContext.cs
@@ -0,0 +1,20 @@
+﻿namespace MiniOrmFramework
+{
+    internal class DbContext
+    {
+        // connection...
+        //Dictionary....db set properties
+
+        internal static readonly Type[] AllowedSqlTypes =
+        {
+            typeof(string),
+            typeof(int),
+            typeof(uint),
+            typeof(long),
+            typeof(ulong),
+            typeof(decimal),
+            typeof(bool),
+            typeof(DateTime),
+        };
+    }
+}
