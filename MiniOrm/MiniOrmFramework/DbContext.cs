d1c1f49fcc5b18505644c45fd8c287fd5bc6b291
Kras13 <paskalev.1989@abv.bg>
2022-05-04 18:56:21 +0300

    DbContext changes.

--- MiniOrm/MiniOrmFramework/DbContext.cs
+++ MiniOrm/MiniOrmFramework/DbContext.cs
@@ -19,10 +19,9 @@ namespace MiniOrmFramework
             typeof(bool),
             typeof(DateTime),
         };
-    }
 
-    public ClassNamePlaceholder(string connectionString)
-    {
+        public DbContext(string connectionString)
+        {
         this.connection = new DatabaseConnection(connectionString);
 
         this.dbSetProperties = this.DiscoverDbSets();
@@ -33,5 +32,110 @@ namespace MiniOrmFramework
         }
 
         this.MappAllRelations();
+        }
+
+        public void SaveChanges()
+        {
+            var dbSets = this.dbSetProperties
+                .Select(pi => pi.Value.GetValue(this))
+                .ToArray();
+
+            foreach(IEnumerable<object> dbSet in dbSets)
+            {
+                var invalidEntities = dbSet
+                    .Where(entity => !IsObjectValid(entity))
+                    .ToArray();
+
+                if(invalidEntities.Any())
+                {
+                    throw new InvalidOperationException(
+                        $"{invalidEntities.Length} Invalid Entities found in {dbSet.GetType().Name!}"
+                    );
+                }
+            }
+
+            using(new ConnectionManager(this.connection))
+            {
+                using(var transaction = this.connection.StartTransaction())
+                {
+                    foreach(IEnumerable dbSet in dbSets)
+                    {
+                        var dbSetType = dbSet.GetType().GetGenericArguments().First();
+
+                        var persistedMethod = typeof(DbContext)
+                            .GetMethod("Persist", BindingFlags.Instance | BindingFlags.NonPublic)
+                            .MakeGenericMethod(dbSetType);
+
+                        try
+                        {
+                            persistedMethod.Invoke(this, new object[] {dbSet});
+                        }
+                        catch(TargetInvocationException tie)
+                        {
+                            throw tie.InnerException;
+                        }
+                        catch(InvalidOperationException)
+                        {
+                            transaction.Rollback();
+                        }
+                        catch(SqlException)
+                        {
+                            transaction.Rollback();
+                            throw;
+                        }
+                    }
+
+                    transaction.Commit();
+                }
+            } 
+        }
+
+        private void Persist<TEntity>(DbSet<TEntity> dbSet)
+            where TEntity : class, new()
+        {
+            var tableName = GetTableName(typeof(TEntity));
+
+            var columns = this.connection.FetchColumnNames(tableName).ToArray();
+
+            if(dbSet.ChangeTracker.Added.Any())
+            {
+                this.connection.InsertEntities(dbSet.ChangeTracker.Added, tableName, columns);
+            }
+
+            var modifiedEntities = dbSet.ChangeTracker.GetModifiedEntities(dbSet).ToArray();
+
+            if(modifiedEntities.Any())
+            {
+                this.connection.UpdateEntities(modifiedEntities, tableName, columns);
+            }
+
+            if(dbSet.ChangeTracker.Removed.Any())
+            {
+                this.connection.DeleteEntities(dbSet.ChangeTracker.Removed, tableName, columns);
+            }
+        }
+
+        private void InitializeObjects()
+        {
+            foreach(var dbSet in this.dbSetProperties)
+            {
+                var dbSetType = dbSet.Key;
+                var dbSetProperty = dbSet.Value;
+
+                var populateDbSetGeneric = typeof(DbContext)
+                    .GetMethod("PopulateDbSet", BindingFlags.Instance | BindingFlags.NonPublic)
+                    .MakeGenericMethod(dbSetType);
+
+                populateDbSetGeneric.Invoke(this, new object[]{dbSetProperty});
+            }
+        }
+
+        private void PopulateDbSet<TEntity>(PropertyInfo dbSet)
+            where TEntity : class, new()
+        {
+            var entities = LoadTableEntities<TEntity>();
+
+            var 
+        }
     }
 }
